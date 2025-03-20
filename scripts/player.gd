adc3d9420c18099f6be2e0b425538cdcae11ae19
gabrielmqc <gabrielmazzo2014@gmail.com>
2025-03-20 16:15:26 -0300

    feat: adding terrain

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_FORCE = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_FORCE
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
