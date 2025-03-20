34c1144ff8dc05698b3070ecda8a16826e5a6658
gabrielmqc <gabrielmazzo2014@gmail.com>
2025-03-20 17:33:20 -0300

    feat: adding walking and jumping animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,23 +3,30 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_FORCE = -400.0
-
-
+var is_jumping := false
+@onready var animation:=$AnimatedSprite2D as AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	elif is_on_floor():
+		is_jumping = false
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
-
+		is_jumping = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("run")
+	elif is_jumping:
+		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
